4574d38b8ce8e7fda11d319c3c41f3d147398de2
승엽 <tmdduqflfl@naver.com>
2025-01-16 17:36:05 +0900

    feat: service에서 controller -> 전달

--- study/src/main/java/com/example/study/ethereum/service/EthereumService.java
+++ study/src/main/java/com/example/study/ethereum/service/EthereumService.java
@@ -1,5 +1,6 @@
 package com.example.study.ethereum.service;
 
+import com.example.study.ethereum.infrastructure.EthereumData;
 import org.springframework.stereotype.Service;
 
 import org.web3j.protocol.Web3j;
@@ -10,8 +11,10 @@ public class EthereumService {
 
     public static final String ETHEREUM_URL = "https://mainnet.infura.io/v3/f344d031dccc418d8f9157e9a4190e1c";
     private final Web3j web3j;
+    private final EthereumData ethereumData;  // 변수 이름 수정
 
-    public EthereumService() {
+    public EthereumService(final EthereumData ethereumData) {
+        this.ethereumData = ethereumData;  // 생성자 변수 이름 수정
         this.web3j = Web3j.build(new HttpService(ETHEREUM_URL));
     }
 
@@ -19,5 +22,8 @@ public class EthereumService {
         String chainId = web3j.ethChainId().send().getChainId().toString();
         System.out.println("연결되었습니다.: " + chainId);
     }
-}
 
+    public void getEthereumPrice(){
+        ethereumData.getEthereumPrice();  // 올바른 변수 사용
+    }
+}
